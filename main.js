562441b19a6ba021e7a999c864df225b956435f5
faraz <silentarrowz@gmail.com>
2017-01-16 18:31:51 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+var submitButton = document.getElementById('submit_button');
+
+submitButton.onclick = function(){
+
+var name=document.getElementById('name');
+var designation = document.getElementById('designation');
+var pan = document.getElementById('pan');
+var aadhar = document.getElementById('aadhar');
+var bank = document.getElementById('bank');
+var basicSalary = document.getElementById('basicsalary');
+var allowance = document.getElementById('allowance');
+var esi = document.getElementById('esi');
+var uan = document.getElementById('uan');
+
+console.log('name is ',name);
+console.log('designation is ',designation);
+console.log('pan is ',pan);
+console.log('aadhar is ',aadhar);
+console.log('bank is ',bank);
+console.log('basicSalary is ',basicSalary);
+console.log('allowance is ',allowance);
+console.log('esi is ',esi);
+console.log('uan is ',uan);
+
+
+
+
+
+}; //submit button function ends
