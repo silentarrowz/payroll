5ac3e9967d680524c062a01a2738e6bf3b6f04ed
faraz <silentarrowz@gmail.com>
2017-01-17 20:57:42 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function (req,res){
+	res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.get('/add-employee.html',function(req,res){
+    res.sendFile(path.join(__dirname,'/add-employee.html'));
+});
+
+app.post('/add-employee.html',function(req,res){
+	
+    console.log('employee '+req.body.name + ' added');
+});
+
+app.post('/form',function (req,res){
+	//employee details from add-employee.html page
+var name=req.body.name;
+var designation = req.body.designation;
+var pan = req.body.pan;
+var aadhar = req.body.aadhar;
+var bank = req.body.bank;
+var basicSalary = req.body.salary;
+var allowance = req.body.allowance;
+var esi = req.body.esi;
+var uan = req.body.uan;
+});
+
+app.get('/add-company.html',function(req,res){
+     res.sendFile(path.join(__dirname,'/add-company.html'));
+});
+
+app.get('/style.css',function(req,res){
+    res.sendFile(path.join(__dirname,'/style.css'));
+});
+
+app.get('/main.js',function(req,res){
+	res.sendFile(path.join(__dirname,'/main.js'));
+});
+
+app.get('/cmain.js',function(req,res){
+	res.sendFile(path.join(__dirname,'/cmain.js'));
+});
+
+var port=8080;
+app.listen(8080,function(req,res){
+   console.log(`Payroll app listening on port ${port}!` );
+});
